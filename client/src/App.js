1d9d503ef9ca081868d2a2a1d03d35452a48a43b
Haikal Khairul <haikalaqimi@gmail.com>
2021-03-24 20:28:30 +0000

    Add components for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Welcome
-      </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
